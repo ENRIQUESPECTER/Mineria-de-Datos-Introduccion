e51c9280e02c546640607f5e73175362dba87d90
ENRIQUESPECTER <luigienrique04@gmail.com>
2026-02-10 18:54:03 -0600

    Add files via upload

---
+++ introduccion.py
@@ -0,0 +1,53 @@
+numero = float(input("INGRESAR NUMERO: "))
+result = numero % 2
+if result == 0:
+    print("EL NUMERO ES PAR :D")
+else:
+    print("EL NUMERO ES IMPAR :0")
+
+
+numero = -69
+if numero > 0:
+    print ("ES POSITIVO")
+elif numero < 0:
+    print ("ES NEGATIVO")
+else:
+    print ("EL NUMERO ES 0")
+
+
+edad = 21
+if edad > 18:
+    print("YA ERES MAYOR DE EDAD D:")
+else:
+    print ("AUN NO ERES MAYOR :0")
+
+
+calificacion = 100
+if calificacion > 60:
+    print ("FELICIDADES APROBASTE :D")
+else:
+    print ("LAMENTABLEMENTE REPROBASTE :C")
+
+
+calificacion = 75
+if calificacion >= 90:
+    print ("TU CALIFICACION ES A")
+elif calificacion >=80:
+    print ("TU CALIFICACION ES B")
+elif calificacion >= 70:
+    print ("TU CALIFICACION ES C")
+elif calificacion >= 60:
+    print ("TU CALIFICACION ES D")
+else:
+    print ("REPROBASTE, TIENES F")
+
+
+temperatura = 150
+if temperatura < 0:
+    print ("LA TEMPERATURA ES SOLIDA")
+elif temperatura >= 0 and temperatura <=100:
+    print("ES LIQUIDO")
+else:
+    print("ES VAPOR")
+
+
